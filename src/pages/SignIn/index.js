44ade533fd78d4d320cff5f2b861c090b58bf82c
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2022-01-03 15:50:26 -0300

    cereate the structure og the login page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+
+import './index.css';
+
+import logo from '../../assets/logo.png';
 
 function SignIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <div>
-            <h1>Pagina de Login</h1>
+        <div className="container-center" >
+            <div className="login" >
+                <div className="logo-area">
+                    <img src={logo} alt="Sistema Logo"/>
+                </div>
+
+                <form action="">
+                    <h1>Entrar</h1>
+                    <input type="text"  placeholder="email@email.com"/>
+                    <input type="password" placeholder="******"/>
+                    <button type="submit">Acessar</button>
+                </form>
+
+                <Link to="/register">Criar uma conta</Link>
+            </div>
         </div>
     );
 };

611b92b825176cc328746d82f89de467cf73afc4
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2022-01-03 20:24:28 -0300

    create submit function in login page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -9,17 +9,32 @@ function SignIn() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        alert('CLICOU');
+    };
+
     return (
         <div className="container-center" >
             <div className="login" >
-                <div className="logo-area">
+                <div className="login-area">
                     <img src={logo} alt="Sistema Logo"/>
                 </div>
 
-                <form action="">
+                <form onSubmit={handleSubmit}>
                     <h1>Entrar</h1>
-                    <input type="text"  placeholder="email@email.com"/>
-                    <input type="password" placeholder="******"/>
+                    <input 
+                     type="text"  
+                     placeholder="email@email.com" 
+                     value={email} 
+                     onChange={(e) => setEmail(e.target.value)} 
+                    />
+                    <input 
+                     type="password" 
+                     placeholder="******"
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                    />
                     <button type="submit">Acessar</button>
                 </form>
 

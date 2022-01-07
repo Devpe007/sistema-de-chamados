c62c671974d2b3006c8221f653a8aede6db71bfc
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2022-01-07 13:37:19 -0300

    create the structure of the Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import { Link } from 'react-router-dom';
+
+import './index.css';
+
+import avatar from '../../assets/avatar.png';
+
+import { FiHome, FiUser, FiSettings } from "react-icons/fi";
+
+import { AuthContext } from '../../contexts/auth';
+
+function Header() {
+    const { user } = useContext(AuthContext);
+
+    return (
+        <div className="sidebar">
+          <div>
+              <img src={user.avatarUrl === null ? avatar : user.avatarUrl} alt="Foto Avatar" />
+          </div>
+
+          <Link to="/dashboard" >
+            <FiHome color="#fff" size={24}/>
+            Chamados
+          </Link>
+
+          <Link to="/dashboard" >
+            <FiUser color="#fff" size={24}/>
+            Clients
+          </Link>
+
+          <Link to="/dashboard" >
+            <FiSettings color="#fff" size={24}/>
+            Configurações
+          </Link>
+        </div>
+    );
+};
+
+export default Header;

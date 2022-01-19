5bf269e5e8700c0798146f574de829216e20a68e
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2022-01-18 22:07:31 -0300

    create the profile page structure

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 
 import './index.css';
 
-import { FiSettings } from 'react-icons/fi';
+import avatar from '../../assets/avatar.png';
+
+import { FiSettings, FiUpload } from 'react-icons/fi';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 
+import { AuthContext } from '../../contexts/auth';
+
 function Profile() {
+    const { user } = useContext(AuthContext);
+
+    const [name, setName] = useState(user && user.name);
+    const [email, setEmail] = useState(user && user.email);
+
+    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
+
     return (
         <div>
             <Header />
@@ -16,6 +27,40 @@ function Profile() {
                 <Title name="Meu Perfil" >
                     <FiSettings size={25} />
                 </Title>
+
+                <div className="container" >
+                    <form className="form-profile" >
+                        <label className="label-avatar">
+                            <span>
+                                <FiUpload color="#fff" size={25} />
+                            </span>
+
+                            <input type="file" accept="image/*" />
+                            <br />
+
+                            { avatarUrl === null 
+                                ? 
+                                <img src={avatar} width="250" height="250" alt="Foto de perfil" />
+                                :
+                                <img src={avatarUrl}  width="250" height="250" alt="Foto de perfil" />
+                            }
+                        </label>
+
+                        <label>Nome</label>
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+
+                        <label>Email</label>
+                        <input type="text" value={email} disabled={true} />
+
+                        <button type="submit" >Salvar</button>
+                    </form>
+                </div>
+
+                <div className="container" >
+                    <button className="logout-btn" >
+                        Sair
+                    </button>
+                </div>
             </div>
         </div>
     );

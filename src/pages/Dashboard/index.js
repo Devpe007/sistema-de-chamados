1f2f82ad381fe0b036b11642d187726859e4ca9e
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2022-01-20 15:00:01 -0300

    create the dashboard page structure

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,43 @@
-import React, { useContext } from "react";
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 
-import { AuthContext } from '../../contexts/auth';
+import './styles.css';
+
+import { FiMessageCircle, FiPlus } from 'react-icons/fi';
 
 import Header from "../../components/Header";
+import Title from '../../components/Title';
 
 function Dashboard() {
-    const { signOut } = useContext(AuthContext);
+    const [calleds, setCalleds] = useState([]);
 
     return (
         <div>
             <Header />
 
-            <h1>PAGINA DASHBOARD</h1>
+            <div className="content" >
+                <Title name="Atendimentos" >
+                    <FiMessageCircle size={25} />
+                </Title>
+
+                {calleds.length === 0 ? (
+                    <div className="container dashboard" >
+                        <span>Nenhum chamado registrado...</span>
+
+                        <Link to="/new" className="new">
+                          <FiPlus size={25} color="#fff" />
+                          Novo Chamado
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        <Link to="/new" className="new">
+                            <FiPlus size={25} color="#fff" />
+                            Novo chamado
+                        </Link>
+                    </>
+                )}
+            </div>
         </div>
     );
 };

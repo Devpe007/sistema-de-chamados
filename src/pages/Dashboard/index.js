062e57d10c4e996f980d3f7ac94d55e80b7e0d46
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2022-01-29 16:26:17 -0300

    create the call search business rule

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
+import { toast } from "react-toastify";
+import { format } from 'date-fns';
 
 import './styles.css';
 
@@ -10,11 +12,77 @@ import {
   FiEdit2,
 } from 'react-icons/fi';
 
+import firebase from "../../connections/firebaseConnection";
+
 import Header from "../../components/Header";
 import Title from '../../components/Title';
 
+const listRef = firebase.firestore().collection('calls').orderBy('created', 'desc');
+
 function Dashboard() {
-    const [calleds, setCalleds] = useState([1]);
+    const [calleds, setCalleds] = useState([]);
+
+    const [loading, setLoading] = useState(true);
+    const [loadingMore, setLoadingMore] = useState(false);
+
+    const [isEmpty, setIsEmpty] = useState(false);
+    
+    const [lastDocs, setLastDocs] = useState();
+
+    useEffect(() => {
+        loadCalls();
+
+        return () => {
+            
+        }
+    }, []);
+
+    async function loadCalls() {
+        await listRef.limit(5)
+         .get()
+         .then((snapshot) => {
+            updateState(snapshot);
+         })
+         .catch((error) => {
+            toast.error('Erro ao carregar chamados!');
+
+            console.log(error);
+            setLoadingMore(false);
+         });
+
+        setLoading(false);
+    };
+
+    async function updateState(snapshot) {
+        const isCollectionEmpty = snapshot.size === 0;
+
+        if(!isCollectionEmpty) {
+            let list = [];
+
+            snapshot.forEach((doc) => {
+                list.push({
+                    id: doc.id,
+                    subjectMatter: doc.data().subjectMatter,
+                    client: doc.data().client,
+                    clientId: doc.data().clientId,
+                    created: doc.data().created,
+                    createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+                    status: doc.data().status,
+                    complement: doc.data().complement,
+                });
+            });
+
+            // Pegando o ultimo documento buscado;
+            const lastDoc = snapshot.docs[snapshot.docs.length -1];
+
+            setCalleds(calleds => [...calleds, ...list]);
+            setLastDocs(lastDoc);
+        } else {
+            setIsEmpty(true);
+        };
+
+        setLoadingMore(false);
+    };
 
     return (
         <div>
